c32e6dee407922084f91773845db84efa61fcd24
Stage UPE1 <stage.upe1@breton.it>
2026-06-03 14:04:08 +0200

    correzzione disposizione cartelle e aggiunta del detector

--- project-root/main.py
+++ project-root/main.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 import os
- 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__,
-            template_folder=os.path.join(os.path.dirname(__file__), '..', 'src', 'ui', 'templates'),
-            static_folder=os.path.join(os.path.dirname(__file__), '..', 'src', 'ui', 'static'))
- 
+            template_folder=os.path.join(BASE_DIR, 'src', 'ui', 'templates'),
+            static_folder=os.path.join(BASE_DIR, 'src', 'ui', 'static'))
+
 @app.route("/")
 def home():
     return render_template("pagina.html")
- 
+
 if __name__ == "__main__":
     app.run(debug=True)
- 
